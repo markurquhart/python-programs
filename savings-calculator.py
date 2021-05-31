b504832a2682aae3bf8a5f148176a91f80c4b1f9
Mark Urquhart <mwurquha@gmail.com>
2021-05-31 14:11:52 -0400

    Update savings-calculator.py
    Added inputs instead of hard-coding the initial values.

--- savings-calculator.py
+++ savings-calculator.py
@@ -1,15 +1,15 @@
 '''Program that calculates savings and interest'''
 
-initial_savings = 5000
-monthly_incremental = 750
-interest_rate = 0.05
+initial_savings = int(input('Enter initial savings amount: '))
+monthly_incremental = int(input('Enter monthly contribution: '))
+interest_rate = float(input('Enter savings rate: '))
 
 years = int(input('Enter years: '))
 print()
 
 savings = initial_savings
 for i in range(years):
-    print(' Savings in year {}: ${:.2f}'.format(i, savings))
+    print(' Savings in year {}: ${:,.2f}'.format(i, savings))
     savings = savings + (savings*interest_rate) + (monthly_incremental*12)
 
 print('\n')
