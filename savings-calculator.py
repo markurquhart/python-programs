0cc1cbc89316db1919b6d02c863c2ea7c986a8c4
Mark Urquhart <mwurquha@gmail.com>
2021-05-31 21:46:34 -0400

    Minor math adjustments

--- savings-calculator.py
+++ savings-calculator.py
@@ -3,6 +3,7 @@
 initial_savings = int(input('Enter initial savings amount ($): '))
 monthly_incremental = int(input('Enter monthly contribution ($): '))
 interest_rate = float(input('Enter savings rate (%): '))/100
+yearly_incremental = monthly_incremental * 12
 
 years = int(input('Enter years: '))
 print()
@@ -10,7 +11,7 @@ print()
 savings = initial_savings
 for i in range(years):
     print(' Savings in year {}: ${:,.2f}'.format(i, savings))
-    # this math indiciates interest that compounds on a yearly basis
-    savings = savings + (monthly_incremental*12) + ((savings+(monthly_incremental*12))*interest_rate)
+    # this math should get us close to what annual, calculated once compound interest would be
+    savings = savings + yearly_incremental + ((savings + yearly_incremental)*interest_rate)
 
 print('\n')
